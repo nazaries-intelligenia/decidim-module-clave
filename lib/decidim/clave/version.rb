1cd3902d94b33c1144735114604cd0609e4cb06a
Alejandro Rueda <alejandro.rueda@nazaries.com>
2025-03-27 11:52:09 +0100

    Update to 0.29.2

--- lib/decidim/clave/version.rb
+++ lib/decidim/clave/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   module Clave
     VERSION = "0.0.3"
-    DECIDIM_VERSION = "0.29.0"
+    DECIDIM_VERSION = "0.29.2"
     COMPAT_DECIDIM_VERSION = [">= 0.29", "< 0.30"].freeze
   end
 end
